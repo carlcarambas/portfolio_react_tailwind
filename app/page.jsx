7b087edcffd4d9d9dbfa5b702d4326694b43d38a
carlcarambaskinesso <carl.carambas@kinesso.com>
2024-06-13 02:06:21 +0800

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+// components
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
