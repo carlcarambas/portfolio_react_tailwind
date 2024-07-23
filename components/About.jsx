74c5eb7819cc4c68a2e54b73d09ce723f2600ecc
Carl Mark Carambas <36352083+carlcarambas@users.noreply.github.com>
2024-07-24 00:04:10 +0800

    tools

--- components/About.jsx
+++ components/About.jsx
@@ -11,6 +11,7 @@ import {
   Phone,
   Briefcase,
 } from 'lucide-react'
+import Image from 'next/image'
 
 const infoData = [
   {
@@ -275,8 +276,52 @@ const About = () => {
                 </TabsContent>
                 {/* skills */}
                 <TabsContent value="skills">
-                  <div>
-                    <h3 className="h3">Tech Stack</h3>
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Tech Stack</h3>
+                    {/* skills */}
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4" />
+                      <div>
+                        {getData(skillData, 'skills').data.map(
+                          (item, index) => {
+                            const { name } = item
+                            return (
+                              <div
+                                key={index}
+                                className="w-2/4 text-center xl:text-left mx-auto xl:mx-0"
+                              >
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            )
+                          }
+                        )}
+                      </div>
+                    </div>
+                    {/* tools */}
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                        Tools
+                      </h4>
+                      {/* tool list */}
+                      <div className="border-b border-border mb-4" />
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, 'tools').data.map((item, index) => {
+                          const { imgPath } = item
+                          return (
+                            <div>
+                              <Image
+                                src={imgPath}
+                                width={48}
+                                height={48}
+                                alt=""
+                                priority
+                              />
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
                   </div>
                 </TabsContent>
               </div>
